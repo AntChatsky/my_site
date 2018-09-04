f44a3c8d80dbedcab4074c8403c3250be791b1dc
AntChatsky <antonyprinzev@gmail.com>
2018-09-04 11:06:36 +0500

    added config

---
+++ app/config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+CSRF_ENABLED = True
+SECRET_KEY = "hedgehogs-are-cute-enough-for-secret-key"
+
+
+class Config(object):
+    FROM_ADDR = "dealsitebot@gmail.com"
+    PASSWORD = "hedgehog322"
